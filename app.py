9154ddccbf63f5196730db750c4c56ca280a5cca
sn1p2r78 <117768713+sn1p2r78@users.noreply.github.com>
2024-04-23 17:53:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def hello_world():
     time.sleep(int(t))
     start0 = datetime.datetime.now(datetime.UTC)
     start = start0.strftime("%Y-%m-%dT%H:00:00")
-    end = start0.strftime("%Y-%m-%dT%H:60:60")
+    end = start0.strftime("%Y-%m-%dT%H:60:00")
     headers = {
                     'Content-Type': 'application/json',
                     'Api-Key': key,
