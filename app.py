6be8d9015985f304a2cccaa1e9275e2b80493d99
sn1p2r78 <117768713+sn1p2r78@users.noreply.github.com>
2024-04-23 17:15:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,42 @@
 import os
 import sys
 import socket
-
+import requsets
 from flask import Flask
 
+import time
+from flask import request
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    version = sys.version_info
-    res = (
-        "<h1>Hello my friends</h1>"
-        f"<h2>{os.getenv('ENV')}</h2></br>"
-        f"Running Python: {version.major}.{version.minor}.{version.micro}<br>"
-        f"Hostname: {socket.gethostname()}"
-    )
-    return res
+    key = request.args.get('key')
+    number = request.args.get('number')
+    start0 = datetime.datetime.now(datetime.UTC)
+    start = start0.strftime("%Y-%M-%dT%H:00:00.999Z")
+    end = start0.strftime("%Y-%M-%dT%H:60:60.999Z")
+    headers = {
+                    'Content-Type': 'application/json',
+                    'Api-Key': key,
+                }
+
+    json_data = {
+                    'id': None,
+                    'jsonrpc': '2.0',
+                    'method': 'sms.mdr_full:get_list',
+                    'params': {
+                        'filter': {
+                            'start_date': start,          
+                            'end_date': end,
+                            'senderid': 'Microsoft',
+                            'phone': number,
+                        },
+                        'page': 1,
+                        'per_page': 1,
+                    },
+                }
+
+    response = requests.post('https://api.premiumy.net/v1.0/csv', headers=headers, json=json_data)return res
